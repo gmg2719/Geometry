9ca0a303be92b19356828273a5ad9eef9d731490
weiyinfu.weiyinfu <weiyinfu.weiyinfu@bytedance.com>
2021-09-06 11:37:53 +0800

    添加一些方法，重命名

--- 求阴影面积/01-出题.py
+++ 求阴影面积/01-出题.py
@@ -20,6 +20,24 @@ x_ind = np.logical_and(x >= 10, x <= w - 10)
 y_ind = np.logical_and(y >= 10, y <= h - 10)
 ind = np.logical_and(x_ind, y_ind)
 img[x[ind], y[ind]] = 1
+"""
+描红
+"""
+A = (w - 10, 10, w - 20)
+B = (cx, cy, (w - 20) // 2)
+
+
+def in_circle(c, p):
+    cx, cy, r = c
+    return np.hypot(p[0] - cx, p[1] - cy) < r
+
+
+for x in range(w):
+    for y in range(w):
+        p = (x, y)
+        if in_circle(B, p) and not in_circle(A, p):
+            img[x][y] = 1
+img = 1 - img
 io.imshow(img)
 plt.axis('off')
 plt.savefig("circle.png")
